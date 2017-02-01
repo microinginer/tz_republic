dfb6261b407690bb6656743b3bd6f9cba6d63227
inginer <ruslanmadatov@yandex.ru>
2017-02-01 21:34:54 +0300

    create post entity

---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contend;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Post
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set contend
+     *
+     * @param string $contend
+     * @return Post
+     */
+    public function setContend($contend)
+    {
+        $this->contend = $contend;
+
+        return $this;
+    }
+
+    /**
+     * Get contend
+     *
+     * @return string 
+     */
+    public function getContend()
+    {
+        return $this->contend;
+    }
+}
