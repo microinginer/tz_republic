187d8e1e6313af61df907c7a116378689daafe7e
inginer <ruslanmadatov@yandex.ru>
2017-02-01 21:27:56 +0300

    move route to routing.yml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,22 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        return $this->render('default/index.html.twig', [
+            'base_dir' => $this->container->getParameter('kernel.root_dir'),
+        ]);
     }
 }
